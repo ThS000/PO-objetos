a34277bb8a3933af63c08f9114ac47ff560a444d
Thiago Saraiva <thiagosaraiva.dev.br@gmail.com>
2026-08-17 12:07:07 -0300

    Exercícios bem simples

--- src/SEMESTRE1/SEMESTRE1_SEMANA5/EX2/Livro.java
+++ src/SEMESTRE1/SEMESTRE1_SEMANA5/EX2/Livro.java
@@ -1,4 +1,27 @@
 package SEMESTRE1.SEMESTRE1_SEMANA5.EX2;
 
 public class Livro {
+     String titulo;
+     String autor;
+     int numPaginas;
+     boolean estaDisponivel;
+
+    public Livro(String titulo, String autor, int numPaginas){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+        this.estaDisponivel = true;
+    }
+
+    public void exibirDetalhes(){
+        System.out.println("Título: " + this.titulo);
+        System.out.println("Autor: " + this.autor);
+        System.out.println("Páginas: " + this.numPaginas);
+        System.out.println("Disponível: " + (this.estaDisponivel ? "Sim" : "Não"));
+
+
+
+
+    }
+
 }
--- src/SEMESTRE1/SEMESTRE1_SEMANA5/EX2/Main.java
+++ src/SEMESTRE1/SEMESTRE1_SEMANA5/EX2/Main.java
@@ -1,4 +1,18 @@
 package SEMESTRE1.SEMESTRE1_SEMANA5.EX2;
 
 public class Main {
+    static void main(String[] args) {
+
+        Livro livro1 = new Livro("O Hobbit", "J.R.R. Tolkien", 310);
+        Livro livro2 = new Livro("Uma Breve História do Tempo", "Stephen Hawking", 256);
+
+        // Alterando o estado do primeiro livro
+        livro1.estaDisponivel = false;
+
+        // Apenas chamando as funções para exibir os dados
+        System.out.println("DETALHES DOS LIVROS");
+        livro1.exibirDetalhes();
+        livro2.exibirDetalhes();
+    }
 }
+
